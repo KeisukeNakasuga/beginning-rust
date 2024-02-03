05df698bfaad3bd4f9e7a6d21199cf230e2b3534
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2024-02-03 12:36:34 +0900

    2章 サンプルコード追加

--- chapter02/src/main.rs
+++ chapter02/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    // 2.1
+    println!("The sum is {}", 71 + 11);
+
+    // 2.5 リテラル文字列の行分け
+    println!("{}", "These \
+    are \
+    three \
+    lines");
 }
