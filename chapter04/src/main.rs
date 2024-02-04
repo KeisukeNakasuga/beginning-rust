ac346665ac5c16ac62d991317e485a7188c85681
KeisukeNakasuga <keisuke.nakasuga@gmail.com>
2024-02-04 11:25:26 +0900

    4章用のプロジェクトを作成 ~ 4章 サンプルコード追加

--- chapter04/src/main.rs
+++ chapter04/src/main.rs
@@ -1,3 +1,63 @@
 fn main() {
-    println!("Hello, world!");
+    // 4.1 if
+    let n = 4;
+    if n > 0 {
+        print!("positive!");
+    } else {
+        print!("not positive");
+    }
+
+    // 4.2 条件式
+    let n = 5;
+    print!("{}",
+        if n > 1000 {
+            "big"
+        }
+        else if n > 0 {
+            "small"
+        }
+        else if n < 0 {
+            "negative"
+        }
+        else {
+            "neither positive nor negative"
+        }
+    );
+
+    // 4.3
+    let mut n = 1;
+    while n <= 10 {
+        print!("{} ", n * n);
+        n += 1;
+    }
+
+    let mut nn = 0;
+    while nn < 50 {
+        nn += 1;
+        if nn % 3 == 0 { continue; }
+        if nn * nn > 400 { break; }
+        print!("{} ", nn * nn);
+    }
+
+    // 4.5 カウント付きループ
+    for n in 1..11 {
+        print!("{} ", n * n);
+    }
+
+    println!("========================");
+
+    for n in 1..=10 {
+        print!("{} ", n * n);
+    }
+
+    println!("========================");
+
+    let mut limit = 4;
+    for n in 1..limit + 2 {
+        limit -= 1;
+        print!("{} {}, ", limit, n);
+    }
+    print!("{}", limit);
+
+    println!("========================");
 }
